bf5b8b12cee21e9e88122564cc438dcff1b163cb
EliasJonsson <ellijons7@gmail.com>
2016-04-22 22:34:08 -0400

    Preprocess updated

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -4,197 +4,8 @@ from string import punctuation
 import nltk
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
 import re
-
-def removeEllipsis(tweets):
-	'''
-	Remove all ellipsis, like ..... ,..,.,. !!.., etc., from the tweets.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without ellipsis 
-
-	'''
-	punct = r'[{}]'.format(re.escape(punctuation))
-	punct = punct + r'{2,}'
-	tweets = [re.sub(punct, "", t) for t in tweets]
-	return tweets
-
-def removeUsers(tweets):
-	'''
-	Remove twitter users, like @jess, @user_1 etc.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without users. 
-
-	'''
-
-	pat = r'((?<=\W)|^)@\S*'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-	return tweets
-
-
-def removeClitics(tweets):
-	'''
-	Remove clitics. Don't, we'll, etc.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets with no clitics.
-	'''
-	pat = r'(^|(?<=\s))\S*[\'`]\S*($|(?=\s))'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-	
-	return tweets
-
-def removePunctuations(tweets):
-	'''
-	Erase all punctuations that are appended to a word, prepended to a word or stands alone.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without punctuations that are appended to a word, prepended to a word, or stands alone.
-
-	'''
-	punct = r'[\.,;:\(\)\[\]\?!#]'
-
-	# Erase all punctuations that are appended to a word.
-	pat = r'((?<=\w)|^)' + punct + r'(?=(\s|$))'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-
-	# Erase all punctuations that are prepended to a word.
-	pat = r'((?<=\s)|^)' + punct + r'(?=(\w|$))'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-
-	# Erase all punctuations that stands alone.
-	pat = r'((?<=\s)|^)' + punct + r'((?=\s)|$)'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-
-	return tweets
-
-def removeHTMLTags(tweets):
-	'''
-	Remove all html tags such as <ref>
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without html tags.
-	'''
-	pat = r'<\S*>'
-	tweets = [re.sub(pat,'', t) for t in tweets]
-	return tweets
-
-def removeURL(tweets):
-	'''
-	Remove all url
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without url.
-	'''
-	pat = r'(www\.|http)\S*\s'
-	tweets = [re.sub(pat, '', t) for t in tweets];
-	return tweets
-
-
-def removePunctuationWords(tweets):
-	'''
-	Remove all words that includes punctuations, except abbreviations.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without words with punctuations except abbreviations.
-	'''
-	punct = r'[{}]'.format(re.escape(punctuation))
-	punct_exception = re.sub(r'[\.#]','',punct)
-	pat = r'(^|\s)\S*' + punct_exception + r'\S*($|\s)'
-	tweets = [re.sub(pat, ' ', t) for t in tweets];
-	return tweets
-
-
-
-def removeShortWords(tweets):
-	'''
-	Remove all single or two letter words.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without single or two letter words.
-	'''
-	pat = r'(^|\s)\S{1,2}(\s|$)'
-	tweets = [re.sub(pat, ' ', t) for t in tweets];
-	return tweets
-	
-def lowercase(tweets):
-	'''
-	Let all words only include lowercase letters.
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets only with lowercase letters.
-	'''
-	tweets = [t.lower() for t in tweets]
-	return tweets
-
-def removeExtraSpaces(tweets):
-	'''
-	Remove all extra spaces
-
-	Input:
-	------------------
-	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
-
-	Output:
-	-----------------
-	newTweets: tweets without extra spaces.
-	'''
-
-	# Erase all double spaces.
-	pat = r'\s{2,}'
-	tweets = [re.sub(pat, " ", t) for t in tweets]
-
-	# Erase all spaces in the begining or in the end of a tweet.
-	pat = r'\s$|^\s'
-	tweets = [re.sub(pat, "", t) for t in tweets]
-
-	return tweets
 
 def splitWords(tweets):
 	'''
@@ -214,8 +25,162 @@ def splitWords(tweets):
 						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
 
 	'''
-	tweets = [t.split() for t in tweets]
+	tknzr = TweetTokenizer()
+	tweets = [tknzr.tokenize(t) for t in tweets]
 	return tweets
+
+def removeHTMLTags(tweets):
+	'''
+	Remove all html tags such as <ref>
+
+	Input:
+	------------------
+	tweets: A list of tweets. [tweet 1, tweet2, ..., tweetN]
+
+	Output:
+	-----------------
+	newTweets: tweets without html tags.
+	'''
+	pat = r'<\S*>'
+	tweets = [[w for w in t if not re.search(pat, w)] for t in tweets]
+	return tweets
+
+def removeUsers(tweets):
+	'''
+	Remove twitter users, like @jess, @user_1 etc.
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets without users. 
+
+	'''
+
+	pat = r'^@\S*'
+	tweets = [[w for w in t if not re.search(pat,  w)] for t in tweets]
+	return tweets
+
+def removeURL(tweets):
+	'''
+	Remove all url
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets without url.
+	'''
+	pat = r'(www\.|http)\S*\s'
+	tweets = [[w for w in t if not re.search(pat, w)] for t in tweets]
+	return tweets
+
+
+def removeEllipsis(tweets):
+	'''
+	Remove all ellipsis, like ..... ,..,.,. !!.., etc., from the tweets.
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets without ellipsis 
+
+	'''
+	punct = r'[{}]'.format(re.escape(punctuation))
+	punct = punct + r'{2,}'
+	tweets = [[w for w in t if not re.search(punct, w)] for t in tweets]
+	return tweets
+
+
+
+def removeShortWords(tweets):
+	'''
+	Remove all single or two letter words.
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets without single or two letter words.
+	'''
+	pat = r'(^|\s)\S{1,2}(\s|$)'
+	tweets = [[w for w in t if not re.search(pat, w)] for t in tweets];
+	return tweets
+	
+def lowercase(tweets):
+	'''
+	Let all words only include lowercase letters.
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets only with lowercase letters.
+	'''
+	tweets = [[w.lower() for w in t] for t in tweets]
+	return tweets
+
+#def removeSpaces(tweets):
+	'''
+	Remove all extra spaces
+
+	Input:
+	------------------
+	tweets: List of lists, [[word1OfTweet1, word2OfTweet1,...,word_m1OfTweet1],
+						   	  [word1OfTweet2, word2OfTweet2,...,word_m2OfTweet2],
+						   						. 								
+						   						. 
+						   						.
+						      [word1OfTweetN, word2OfTweetN,...,word_mNOfTweetN]]
+
+	Output:
+	-----------------
+	newTweets: tweets without extra spaces.
+	'''
+
+	# Erase all spaces.
+#	pat = r'^\s{1,}$'
+#	tweets = [[w for w in t if not re.search(pat,t)] for t in tweets]
+
+#	return tweets
+
+
 
 def removeStopwords(tweets):
 	'''
@@ -268,7 +233,6 @@ def lemmatize(tweets):
 					pass
 		tweets.append(tt)
 		i += 1
-		print i
 	return tweets
 
 
@@ -293,7 +257,6 @@ def remove_infrequent_words(tweets):
 	df = Counter()
 	for t in tweets:
 		df += Counter(set(t))
-	print df
 	tweets = [[w for w in t if df[w]>cap]for t in tweets]
 	return tweets
 
@@ -329,21 +292,20 @@ def preprocess(tweets):
 	-----------------
 	newTweets: tweets ready for the training of topic models.
 	'''
-	tweets = removeEllipsis(tweets)
-	tweets = removeUsers(tweets)
+	tweets = splitWords(tweets)
 	tweets = removeHTMLTags(tweets)
+	tweets = removeUsers(tweets)
 	tweets = removeURL(tweets)
-	tweets = removeClitics(tweets)
-	tweets = removePunctuations(tweets)
-	tweets = removePunctuationWords(tweets)
+	tweets = removeEllipsis(tweets)
 	tweets = removeShortWords(tweets)
 	tweets = lowercase(tweets)
-	tweets = removeExtraSpaces(tweets)
-	tweets = splitWords(tweets)
+	#tweets = removeSpaces(tweets)
 	tweets = removeStopwords(tweets)
+	print tweets
 	tweets = lemmatize(tweets)
 	tweets = remove_infrequent_words(tweets)
 	tweets = remove_short_tweets(tweets)
+	print tweets
 	
 	return tweets
 
