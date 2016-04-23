0ba7f2885541c11b6c5bb00defe705d387bd88f8
EliasJonsson <ellijons7@gmail.com>
2016-04-22 20:20:43 -0400

    updated pp.py file. Now it is possible to leave out the argument to use the entire corpus.

--- Code/pp.py
+++ Code/pp.py
@@ -4,9 +4,9 @@ import numpy as np
 import preprocess, time, pickle, sys
 #file_name = '../Data/testdata.manualSUBSET.2009.06.14.csv'
 file_name = '../Data/training.1600000.processed.noemoticon.csv'
-n = int(sys.argv[1])
 tweets = loadData(file_name)
-tweets = tweets[0:n]
+if len(sys.argv) > 1:
+	tweets = tweets[0:int(sys.argv[1])]
 tweets = [tweets[i] for i in np.random.permutation(len(tweets))]
 tweets = preprocess.preprocess(tweets)
 pickle.dump( tweets, open( "preprocessedData.pkl", "wb" ) )
